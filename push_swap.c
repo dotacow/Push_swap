cfdd8d3a81d8ece6cfea0d6eb361127611b12e91
Yousef Kitaneh <yokitane@c2r2s3.42amman.com>
2024-10-28 16:25:14 +0300

    fixed leaks and norm

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,16 @@
 /*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 17:33:46 by yokitane          #+#    #+#             */
-/*   Updated: 2024/10/28 16:01:43 by yokitane         ###   ########.fr       */
+/*   Updated: 2024/10/28 16:24:49 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 // build the sa,sb,sss,ra,rb,rr,rra,rrb,rrr
+//do 5 and below
+	//2 /3 /4 /5
+//bubble sort
 // work on algorithim
 int	main(int argc, char **argv)
 {
