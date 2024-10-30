805736a539d1d2c4a55a9b994ff0cfa04af5f29d
dotacow <dotacowidkmynamesry@gmail.com>
2024-10-30 15:55:21 +0300

    added rotate operations

--- opsrcs/ft_rotate.c
+++ opsrcs/ft_rotate.c
@@ -6,8 +6,43 @@
 /*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 11:37:06 by yokitane          #+#    #+#             */
-/*   Updated: 2024/10/30 11:38:23 by yokitane         ###   ########.fr       */
+/*   Updated: 2024/10/30 15:52:55 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+static void	ft_rotate(t_ilist **stack)
+{
+	t_ilist	*tmp;
+	t_ilist	*head;
+
+	if (!*stack || !((*stack)->next))
+		return ;
+	head = *stack;
+	tmp = head;
+	*stack = tmp->next;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = head;
+	head->next = NULL;
+}
+
+void	ft_rotate_a(t_ilist **stack_a)
+{
+	ft_rotate(stack_a);
+	ft_printf("ra\n");
+}
+
+void	ft_rotate_b(t_ilist **stack_b)
+{
+	ft_rotate(stack_b);
+	ft_printf(rb\n);
+}
+
+void	ft_rotate_r(t_ilist **stack_a, t_ilist **stack_b)
+{
+	ft_rotate(stack_a);
+	ft_rotate(stack_b);
+	ft_printf("rr\n");
+}
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 10:41:27 by dotacow           #+#    #+#             */
-/*   Updated: 2024/10/28 17:22:59 by yokitane         ###   ########.fr       */
+/*   Updated: 2024/10/30 15:53:49 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,7 @@ void	ft_ilstadd_back(t_ilist **lst, t_ilist *new);
 void	ft_ilstclear(t_ilist **lst);
 void	ft_ilstprint(t_ilist *lst);
 void	ft_free(char **words);
-
+void	ft_rotate_a(t_ilist **stack_a);
+void	ft_rotate_b(t_ilist **stack_b);
+void	ft_rotate_r(t_ilist **stack_a, t_ilist **stack_b);
 #endif
