f3b3efb6485d38579a736ea0b864879a2fae0e56
Yousef Kitaneh <yokitane@c2r4s3.42amman.com>
2024-10-31 21:08:03 +0300

    f

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 17:33:46 by yokitane          #+#    #+#             */
-/*   Updated: 2024/10/31 20:49:28 by yokitane         ###   ########.fr       */
+/*   Updated: 2024/10/31 21:01:36 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,24 @@
 int	main(int argc, char **argv)
 {
 	t_ilist	**stack_a;
-	// t_ilist	**b;
+	t_ilist	**stack_b;
+
 	if (argc == 1)
 		return (0);
 	stack_a = getlist(argc, argv);
+	stack_b = NULL;
 	if (!stack_a)
 	{
 		write(2, "Error\n", 6);
 		return (1);
 	}
-	ft_sa(stack_a);
-	ft_ra(stack_a);
+	ft_printf("stack a:\n");
 	ft_ilstprint(*stack_a);
-	ft_ilstclear(stack_a);
-	// b = NULL;
-	// sort(a, b);
+	ft_pb(stack_a,stack_b);
+	// ft_ilstclear(stack_a);
+	// ft_printf("stack a:\n");
+	// ft_ilstprint(*stack_b);
+	// ft_ilstclear(stack_b);
+	// // sort(a, b);
 	return (0);
 }
