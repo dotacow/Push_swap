46d2d53f867fb734b0338e8f845703c4b8af58d5
dotacow <dotacowidkmynamesry@gmail.com>
2024-11-01 11:56:27 +0300

    fixed seg fault

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 17:33:46 by yokitane          #+#    #+#             */
-/*   Updated: 2024/10/31 21:01:36 by yokitane         ###   ########.fr       */
+/*   Updated: 2024/11/01 11:55:55 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,19 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 		return (0);
 	stack_a = getlist(argc, argv);
-	stack_b = NULL;
-	if (!stack_a)
+	stack_b = malloc(sizeof(t_ilist *));
+	*stack_b = NULL;
+	if (!stack_a || !stack_b)
 	{
 		write(2, "Error\n", 6);
 		return (1);
 	}
 	ft_printf("stack a:\n");
-	ft_ilstprint(*stack_a);
-	ft_pb(stack_a,stack_b);
+	ft_pb(stack_a, stack_b);
+	ft_pa(stack_a, stack_b);
+	ft_ilstprint(*stack_b);
 	// ft_ilstclear(stack_a);
-	// ft_printf("stack a:\n");
-	// ft_ilstprint(*stack_b);
 	// ft_ilstclear(stack_b);
-	// // sort(a, b);
+	free(stack_b);
 	return (0);
 }
